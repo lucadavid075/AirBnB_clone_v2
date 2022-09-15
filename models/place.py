68d041ad4b8ed01a1908ad7c655c033d84f45003
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-15 01:37:59 +0100

    hbnb
    unbroke the code

--- models/place.py
+++ models/place.py
@@ -7,14 +7,13 @@ from models.city import City
 from models.user import User
 from sqlalchemy.orm import relationship
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60), ForeignKey(
-                              'places.id'), primary_key=True, nullable=False),
-                          Column('amenity_id', String(60), ForeignKey(
-                              'amenities.id'), primary_key=True,
-                              nullable=False)
-                          )
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey(
+                          'places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey(
+                          'amenities.id'), primary_key=True,
+                          nullable=False)
+                      )
 
 
 class Place(BaseModel, Base):
