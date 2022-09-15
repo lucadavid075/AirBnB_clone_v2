44a993c209f7894aeca6a4f88b9476a903e60139
Luke David <daiveedlucas049@gmail.com>
2022-09-15 04:35:40 +0000

    [testing]

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Unit tests for DB storage"""
+''' Unit tests for DB storage '''
 import os
 import unittest
 import models
@@ -11,43 +11,55 @@ from models.state import State
 
 @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
                  "Only want to test Database storage")
-class TestDBStorage(unittest.TestCase):
-    """Testing the DB storage class"""
+class testDBStorage(unittest.TestCase):
+    '''
+    Testing the DB storage class
+    '''
     def test_existence_user(self):
-        """Testing if User class is being created properly"""
-        user = User(email="name@email.com", password="passpwd")
+        '''
+        Testing if User class is being created properly
+        '''
+        user = User(email="john@snow.com", password="johnpwd")
         user.save()
         if user.id in models.storage.all('User'):
-            self.assertTrue(user.password, "passpwd")
+            self.assertTrue(user.password, "johnpwd")
 
     def test_existence_amenity(self):
-        """Testing if Amenity class is being created properly"""
+        '''
+        Testing if Amenity class is being created properly
+        '''
         amenity = Amenity(name="Wifi")
         amenity.save()
         if amenity.id in models.storage.all():
             self.assertTrue(amenity.name, "Wifi")
 
     def test_existence_state(self):
-        """Testing if State class is being created properly"""
-        state = State(name="Kenya")
+        '''
+        Testing if State class is being created properly
+        '''
+        state = State(name="Alaska")
         state.save()
         if state.id in models.storage.all():
-            self.assertTrue(state.name, "Kenya")
+            self.assertTrue(state.name, "Alaska")
 
     def test_all_method(self):
-        """Testing if all() method returns all instances"""
+        '''
+        Testing if all() method returns all instances
+        '''
         state = State(name="Cali")
         state.save()
         amenity = Amenity(name="Cable")
         amenity.save()
-        user = User(email="name@email.com", password="passpwd")
+        user = User(email="john@snow.com", password="johnpwd")
         user.save()
         test_me = str(state.id) + str(amenity.id) + str(user.id)
         if test_me in models.storage.all():
             self.assertTrue(state.name, "Cali")
 
     def test_delete_method(self):
-        """Tests the delete method in db_storage"""
+        '''
+            Tests the delete method in db_storage
+        '''
         state = State(name="Texas")
         state.save()
         all_stored = models.storage.all()
