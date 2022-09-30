50019668b64cab7c68f8fa4cf11d60a7c652b533
Luke David <daiveedlucas049@gmail.com>
2022-09-30 01:11:38 +0000

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,5 +8,6 @@ sudo touch /data/web_static/releases/test/index.html
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-sudo sed -i "38i\ \tlocation \/hbnb_static {\\n\\t\\talias \/data\/web_static\/current\/;\\n}" /etc/nginx/sites-available/default
+serverblock="server_name\ _;\\n\\n\\tlocation\ \/hbnb_static\/\ {\\n\\t\\talias\ \/data\/web_static\/current\/;\\n\\t}\\n"
+sudo sed -i "s/server_name _;/$serverblock/" /etc/nginx/sites-available/default
 sudo service nginx restart
