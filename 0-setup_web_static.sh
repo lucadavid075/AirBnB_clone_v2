4de9ff82aea6537c52bb6aafe9252f26cf594273
lucadavid075 <daiveedlucas049@gmail.com>
2022-10-14 03:52:04 -0700

    static page task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,7 @@ sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
+sudo chown -R root:root /data
 serverblock="server_name\ _;\\n\\n\\tlocation\ \/hbnb_static\/\ {\\n\\t\\talias\ \/data\/web_static\/current\/;\\n\\t}\\n"
 sudo sed -i "s/server_name _;/$serverblock/" /etc/nginx/sites-available/default
 sudo service nginx restart
