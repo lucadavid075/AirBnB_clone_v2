700d9477ab45a30e07c4ece5b47217a0b9a793a2
Luke David <daiveedlucas049@gmail.com>
2022-10-15 05:00:59 +0000

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # script that starts a flask web application
-
-
 from flask import Flask, request
 from markupsafe import escape
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
